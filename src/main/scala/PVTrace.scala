d0e1ea5b9c67add326add5f2b2b0aecf701595a7
Weihua Jiang <whjiang@outlook.com>
2016-12-22 09:36:12 +0800

    make compilable

--- src/main/scala/PVTrace.scala
+++ src/main/scala/PVTrace.scala
@@ -1,36 +1,39 @@
-import org.apache.flink.api.common.time.Time
-import org.apache.flink.streaming.api.scala.DataStream
+
+import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
 import org.apache.flink.streaming.api.windowing.triggers.ContinuousProcessingTimeTrigger
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
-import org.apache.flink.streaming.runtime.operators.windowing.WindowOperator.Context
 import org.apache.flink.util.Collector
+import com.voop.data.cleaning.logic.mars.mobile.page.MobilePageProtos.MobilePage
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+import org.apache.flink.streaming.api.windowing.time.Time
+
 
 /**
-  * Created by whjiang on 2016/12/21.
+  * main handling logic
   */
 class PVTrace {
   def genPVTrace(dataStream: DataStream[MobilePage]): DataStream[UserPVTraceLog] = {
     dataStream
       .keyBy(_.getUserid)
-      .window(EventTimeSessionWindows.withGap(Time.minutes(60))
+      .window(EventTimeSessionWindows.withGap(Time.minutes(60)))
       .trigger(ContinuousProcessingTimeTrigger.of(Time.seconds(5)))
       .apply(new SortAndEmitFn)
 
   }
-
-  private def getEventTime(mobilePage: MobilePage): Long = {
-    mobilePage.getPageStartTime
-  }
 }
 
-class SortAndEmitFn extends ProcessWindowFunction[MobilePage, UserTrajectory, String, TimeWindow] {
+class SortAndEmitFn extends ProcessWindowFunction[MobilePage, UserPVTraceLog, String, TimeWindow] {
 
   override def process(userId: String, input: Iterable[MobilePage],
                        context: Context,
                        out: Collector[UserPVTraceLog]): Unit = {
     val trajectory = input.filter(getEventTime(_) <= context.watermark).toList.sortBy(getEventTime)
-    out.collect(UserPVTraceLog(userId, trajectory, context.window, context.watermark))
+    out.collect(UserPVTraceLog(userId, 0, trajectory(0)))
+  }
+
+  private def getEventTime(mobilePage: MobilePage): Long = {
+    mobilePage.getPageStartTime
   }
 }
 
